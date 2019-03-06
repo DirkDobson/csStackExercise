831a8bb886a83d37f4a28632b01d749a166f753d
Dirk Dobson <dirkvancouver@live.com>
2019-03-05 17:37:16 -0700

    methods

--- Stack.cs
+++ Stack.cs
@@ -4,6 +4,18 @@ namespace Stack
 {
     class Stack
     {
+      public void Push( object obj)
+      {
 
+      }
+      public void Pop()
+      {
+
+      }
+
+      public void Clear()
+      {
+
+      }
     }
 }
