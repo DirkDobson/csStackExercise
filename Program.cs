b15492a5a7e0a0fbaebf0762b4e31f959ce51f5c
Dirk Dobson <dirkvancouver@live.com>
2019-03-09 22:28:31 -0700

    program

--- Program.cs
+++ Program.cs
@@ -8,6 +8,18 @@ namespace Stack
         {
             System.Console.WriteLine("what would you lke to do.");
             string user =  Console.ReadLine();
+            if(user == "push")
+            {
+                Stack.push();
+            }
+            if (user == "pop")
+            {
+                Stack.Pop();
+            }
+            if (user == "clear")
+            {
+                Stack.clear();
+            }
         }
     }
 }
